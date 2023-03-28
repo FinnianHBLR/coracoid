c4b664e8dd007442800785713065eba581c90acb
FinnianHBLR <finnniansproject@gmail.com>
2023-03-28 17:54:20 +1300

    Added update, likes and font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app'; 
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
+
+import { doc, updateDoc } from "firebase/firestore";
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -26,11 +28,12 @@ function App() {
     <header>
       <h1>Coracoid ://</h1>
       <p className='feedTitle'>Global Feed</p>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand"></link>
     </header>
     <div className='side-container'>
     <UserInfo/>
+    {user ? <PopUpNewPost/>: <null></null>}
     <SignOut/>
-    {user ? <PopUpNewFeather/>: <null></null>}
     {user ? <null></null> : <SignIn/>}
     
     </div>
@@ -43,31 +46,31 @@ function App() {
   );
 }
 
-function PopUpNewFeather() {
+function PopUpNewPost() {
   return (
 <Popup
-    trigger={<button className="button"> New Feather </button>} modal nested>
+    trigger={<div className='new-post-btn-container'><button className="new-post-btn"> New Post </button></div>} modal nested>
     {close => (
       <div className="modal">
         <button className="close" onClick={close}>
           &times;
         </button>
-        <div className="header"> Modal Title </div>
+        <div className="header"> Create a new post </div>
         <div className="content">
           {' '}
-          What would you like to say?
+          <p>What would you like to say?</p>
           <PopBoxInfo></PopBoxInfo>
         </div>
         
         <div className="actions">
           <button
-            className="button"
+            className="cancel-post-button"
             onClick={() => {
               console.log('modal closed ');
               close();
             }}
           >
-            Close Feather
+            Cancel Post
           </button>
         </div>
 
@@ -84,10 +87,10 @@ function PopBoxInfo() {
   const [formValue, setFormValue] = useState('');
   // Set uid, photoURL and display name to be used to send a message.
   const {uid, photoURL, displayName} = auth.currentUser
-
-  // Send Feather method
-  const sendFeather = async(e) =>{
-    // Prevent reload on message send. Send Feather sends the message.
+  const likes = 0;
+  // Send Post method
+  const sendPost = async(e) =>{
+    // Prevent reload on message send. Send Post sends the message.
     e.preventDefault();
 
     // When a message is created add user info and message text.
@@ -96,19 +99,34 @@ function PopBoxInfo() {
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
       photoURL,
-      displayName
-    })
-    setFormValue('');
+      displayName,
+      likes,
+      likedBy: [],
+      customID: '',
+      likedBy: []
+    }).then((createdRef) => {
+      // DEBUG: console.log(value.id);
+      
+      // Get ID of object and set it to customID so it can be used to locate to be edited.
+      createdRef.update({
+        customID: createdRef.id
+      });
+    });
 
+
+    // Reset form
+    setFormValue('');
+    // Move to new message
     window.scrollTo(0, 0);
   }
 
   // For definition of form
   return (
     <div>
-    <form onSubmit={sendFeather}>
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
-      <button type='submit'>Create a new Feather</button>
+    <form onSubmit={sendPost}>
+      {/*formValue is used in messageRef ^ */}
+      <textarea className='input-field' placeholder="Enter text..." value={formValue} onChange={(e) => setFormValue(e.target.value)}/>  
+        <button className='post-send-btn' type='submit'>Create a new Post</button>
     </form>
     </div>
   )
@@ -121,7 +139,6 @@ function UserInfo(){
       <img className='bioImg' src={auth.currentUser.photoURL}></img>
       <p className='bioInfo'>Hello {auth.currentUser.displayName}!</p>
       <p className='bioInfo'>Email: {auth.currentUser.email}</p>
-      <p className='bioInfo'>Ph: {auth.currentUser.phoneNumber}</p>
     </div>
   )
 
@@ -154,26 +171,42 @@ function GlobalFeed (){
   const dummy = useRef();
   // Access the messages on Firebase.
   const messagesRef = firestore.collection('messages');
-  // Oreder messages by timestamp in a descending order
+    // Oreder messages by timestamp in a descending order
   const query = messagesRef.orderBy('createdAt', 'desc').limit(100);
-  const [messages] = useCollectionData(query, {idField: 'id'})
+
+  const options = {idField: 'id'}; // Add this line to specify the idField
+  const [messages] = useCollectionData(query, options);
 
   return (
     <main>
     <div>
-      {messages && messages.map(msg => <Feather key={msg.id} messages={msg}/>)}
+      {messages && messages.map(msg => <Post key={msg.id} messages={msg} />)}
       <div ref={dummy}></div>
     </div>
     </main>
   )
 }
 
-function Feather(props){
-  // A feather is a message box.
-  const {text, uid, photoURL, displayName} = props.messages;
+
+function Post(props){
+  // A Post is a message box.
+  const {text, uid, photoURL, displayName, likes, customID, likedBy} = props.messages;
   // Checks if the current user is sending the message, change CSS class if that is true.
   const messageClass = uid === auth.currentUser.uid ? 'send' : 'received';
 
+  const messagesRef = firebase.firestore().collection('messages');
+
+  const likePost = async(e) => {
+    const docRef = messagesRef.doc(customID);
+    await docRef.update({
+      likes: likes+1,
+
+      // WIP ----------
+      likedBy: likedBy.push(auth.currentUser.displayName)
+    });
+  }
+  
+  
   return (
     <div className={`message ${messageClass}`}>
       <img className='messageUserImg' src={photoURL}/>
@@ -181,8 +214,12 @@ function Feather(props){
       <div className='chat-container'>
         <p className='chat-container-content'>{text}</p>
       </div>
-      <button className='chatLikeBtn'>Like</button>
-        <p className='chatNumberLike'>Numper of likes: 0</p>
+      <button onClick={likePost} className='chatLikeBtn'>Like</button>
+        <p className='chatNumberLike'>Numper of likes: {likes}</p>
+        <p>ID: {customID}</p>
+        {/* WIP ---------------- */}
+        {likedBy && likedBy.map(users => <div> <p>{users}</p> </div>)}
+
     </div>
   )
   
